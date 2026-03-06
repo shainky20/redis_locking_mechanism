0101202c5a3664488cbf6644858e4832c08d02ad
shainky20 <shainkysoni47@gmail.com>
2026-03-06 23:25:14 +0530

    Delete src directory

--- src/routes/lock.routes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { acquireLock, releaseLock } from "../controllers/lock.controller.js";
-
-const router = express.Router();
-
-router.post("/acquire", acquireLock);
-router.post("/release", releaseLock);
-
-export default router;
